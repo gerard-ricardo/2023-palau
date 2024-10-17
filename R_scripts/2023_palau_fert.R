24b03136e4a33eaf778b515c5544e09e28d3cab0
Gerard Ricardo <51186451+gerard-ricardo@users.noreply.github.com>
2024-10-17 16:46:53 +1000

    clones and fert

--- R_scripts/2023_palau_fert.R
+++ R_scripts/2023_palau_fert.R
@@ -1,7 +1,10 @@
 # 2023 palau fert
 
-## notes
-# - need to crrect closer to water flow. containers at 142 degrees
+
+# notes -------------------------------------------------------------------
+# - gamm might be slightly better fitted vs resid 
+# outlier at distnace = 30, deg = 25 is likely from two spokes crossing, check genetics
+
 
 
 
@@ -9,19 +12,18 @@
 library(tidyverse)
 library(ggplot2)
 library(tidyr)
-source("https://raw.githubusercontent.com/gerard-ricardo/data/master/theme_sleek2") # set theme in code
+library(glmmTMB)
 library(lubridate)
+source("https://raw.githubusercontent.com/gerard-ricardo/data/master/theme_sleek2") # set theme in code
 
 
 # 1 Import data -----------------------------------------------------------
-# read.excel <- function(header = TRUE, ...) {
-#   read.table("clipboard", sep = "\t", header = header, na.strings = c("", "-", "na"), ...)
-# }
-# data1 <- read.excel() # read clipboard from excel
-# save(data1, file = file.path("./Rdata", "2023_palau_fert.RData"))
+# # read.excel <- function(header = TRUE, ...) {
+# #   read.table("clipboard", sep = "\t", header = header, na.strings = c("", "-", "na"), ...)
+# # }
+# # data1 <- read.excel() # read clipboard from excel
+# # save(data1, file = file.path("./Rdata", "2023_palau_fert.RData"))
 load("./Rdata/2023_palau_fert.RData")
-
-
 
 # 2. Labelling and wrangling -----------------------------------------------
 str(data1) # check data type is correct
@@ -29,60 +31,53 @@ data1$spoke <- as.factor(as.character(data1$spoke))
 data1$id <- as.factor(as.character(data1$id))
 data1$obs <- factor(formatC(1:nrow(data1), flag = "0", width = 3)) # unique tank ID for later on
 data1$prop <- data1$suc / data1$tot
-# Convert to POSIXct, assuming all times are PM
 data1$time <- sprintf("%s PM", data1$time)  # Append PM to each time string
 data1$time <- as.POSIXct(paste("2000-01-01", data1$time), format="%Y-%m-%d %I:%M %p", tz="UTC")
 base_time <- min(data1$time, na.rm = T)
 data1$time_from_base <- as.numeric(difftime(data1$time, base_time, units = "secs"))
 data1$quality_score = 1
-# adjust the quality_score based on specific conditions in the dataframe
+data1 <- data1[!is.na(data1$prop), ]
+data1$suc = ifelse(data1$suc == 0, 1, data1$suc)  #try adding small error to work betabinom
+
+# adjust the quality_score based on specific conditions in the dataframe (not on atm)
 data1$quality_score[data1$id == "5_05" & data1$spoke == 5] <- 0.5  # Set quality_score to 50 for id 5_05 and spoke 5
 data1$quality_score[data1$id == "7_05" & data1$spoke == 7] <- 0.5  # Set quality_score to 50 for id 7_05 and spoke 7
 
-#save(data1, file = file.path("./Rdata", "fert_data.RData"))
-load("./Rdata/fert_data.RData") #data1
 
 # 3. Data Exploration ----------------------------------------------------
 
 # Wrangling
-data2 <- data1[25:28, ] # only centre
-data3 <- data1[1:24, ] # only radial lines
+centre_indiv <- data1[which(data1$spoke == 'c'),]
+radial_indiv <- data1[which(data1$spoke != 'c'),]
 
 # Calculate degrees from a baseline (s1) and adjust by subtracting an offset (s5)
-deg_from_s1 <- c(26, 52, 78, 104, 130, 156)
-deg_from_north  = c(77, 108, 124, 148, 173, 201) #taken from earth (radial 2:7)
-(deg_from_s5 <- deg_from_s1 - 104)   #146.5 deg . 
-deg_from_water_dir = deg_from_north - 146.5
-data3$deg <- sort(rep(deg_from_water_dir, 4))
+# radial_indiv <- radial_indiv %>%
+#   mutate(deg_from_s1 = case_when(spoke == 2 ~ 26,spoke == 3 ~ 52,spoke == 4 ~ 78,spoke == 5 ~ 104,spoke == 6 ~ 130,spoke == 7 ~ 156,TRUE ~ NA_real_  # Assign NA for any other values
+#   ))
 
-##overall mean
+# Calculate degrees from north
+radial_indiv <- radial_indiv %>%
+  mutate(deg_from_north = case_when(spoke == 2 ~ 77, spoke == 3 ~ 108, spoke == 4 ~ 124, spoke == 5 ~ 148, spoke == 6 ~ 173,spoke == 7 ~ 201, TRUE ~ NA_real_  # Assign NA for any other values
+  ))  #taken from earth 
+#(deg_from_s5 <- deg_from_s1 - 104)   
+radial_indiv$deg = radial_indiv$deg_from_north - 146.5   #adjust to water dir
+range(radial_indiv$deg )
+radial_indiv$deg_rad <- radial_indiv$deg * pi / 180
+radial_indiv$sin_deg <- sin(radial_indiv$deg_rad)
+radial_indiv$cos_deg <- cos(radial_indiv$deg_rad)
+
+## overall mean and weighted means
 # sum of all embryos
-(with(data1, sum(suc, na.rm = T) / sum(tot, na.rm = T)))  #22.4%
+(with(data1, sum(suc, na.rm = T) / sum(tot, na.rm = T)))  #0.235
 #mean of all samples
 with(data1, mean(prop, na.rm = T))  #unweighted
 #weighted mean
 (wei_mean_prop_cent <- sum(data1$prop * data1$tot, na.rm = TRUE) / sum(data1$tot, na.rm = TRUE))
-(wei_mean_prop_cent <- sum(data2$prop * data2$tot, na.rm = TRUE) / sum(data2$tot, na.rm = TRUE))
-(wei_mean_prop_rad <- sum(data3$prop * data3$tot, na.rm = TRUE) / sum(data3$tot, na.rm = TRUE))
+(wei_mean_prop_cent <- sum(centre_indiv$prop * centre_indiv$tot, na.rm = TRUE) / sum(centre_indiv$tot, na.rm = TRUE))  #centre
+(wei_mean_prop_rad <- sum(radial_indiv$prop * radial_indiv$tot, na.rm = TRUE) / sum(radial_indiv$tot, na.rm = TRUE))  #spokes
 
 
-
-# #quick data input (need to check)
-# x  = c(0.7, 3, 10, 30)
-# deg_from_s1 = c(26, 52, 78, 104, 130, 156)
-# (deg_from_s5 = deg_from_s1 - 104)
-# spoke2 = c(0,1,2,NA)
-# spoke3 = c(9,1,0,0)
-# spoke4 = c(75, 26, 30, 13)
-# spoke5 = c(74, NA, 84, 0)
-# spoke6 = c(92, NA, 20, 65)
-# spoke7 = c(90, 54, 9, 2)
-# all_spokes = c(spoke2, spoke3, spoke4, spoke5, spoke6, spoke7)
-# length(dist)
-# data1 = data.frame(suc = all_spokes, tot = 100, dist = rep(x, 6), deg = sort(rep(deg_from_s5, 4)), spoke = sort(rep(c('s2', 's3', 's4', 's5', 's6', 's7'), 4)))
-
-
-# 4. Modelling ------------------------------------------------------------
+# Modelling ------------------------------------------------------------
 # library(lme4)
 # md1 <- glmer(cbind(suc, (tot - suc)) ~ scale(dist) * scale(deg) + (1 | obs), family = binomial, data = data1)
 
@@ -94,38 +89,93 @@ with(data1, mean(prop, na.rm = T))  #unweighted
 # summary(md1)
 # plot(fitted(md1), resid(md1)) # fitted vs residuals
 # abline(h = 0)
+#devtools::install_version("glmmTMB", version = "1.1.08", repos = "http://cran.us.r-project.org")
 
-library(glmmTMB)
-md1 <- glmmTMB(cbind(suc, (tot - suc)) ~ (dist) + poly(deg, 2) + (1 | obs), family = "betabinomial", weights = quality_score, data = data3)
+str(radial_indiv)
+radial_indiv$cos_deg
+levels(radial_indiv$obs)
+md1 <- glmmTMB(cbind(suc, (tot - suc)) ~ scale(dist) + poly(deg, 2) + (1 | obs), family = "betabinomial", data = radial_indiv)
 summary(md1)
 AIC(md1)
 plot(fitted(md1), resid(md1)) # fitted vs residuals
 abline(h = 0)
 ## betabiomial better fitted vs resid
 
-# mes
 
-# 1. Generate a new data frame for predictions
+## technically angles should be in radians (sin and cos) or dealt with a gam (see below0
+# library(mgcv)
+# library(gamm4)
+# md1 <- gamm4(cbind(suc, tot - suc) ~ s(deg, bs = "cc") + dist + (1 | obs), family = binomial, data = radial_indiv)
+# md1 <- glmmTMB(cbind(suc, tot - suc) ~ 
+#                  s(deg, bs = "cc", k = 4) + 
+#                  s(dist, k = 4) + 
+#                  (1 | obs), 
+#                family = betabinomial(),
+#                data = radial_indiv)
+
+
+# radian
+# angles_deg <- seq(min(radial_indiv$deg), max(radial_indiv$deg), length.out = 100)  # -69.5 to 54.5 degrees
+# angles_rad <- angles_deg * (pi / 180)  # Convert to radians
+# sin_deg <- sin(angles_rad)
+# cos_deg <- cos(angles_rad)
+
+# Generate new_data for predictions
 new_data <- expand.grid(
-  dist = seq(min(data3$dist), max(data3$dist), length.out = 100),
-  deg = seq(min(data3$deg), max(data3$deg), length.out = 100),
+  dist = seq(min(radial_indiv$dist), max(radial_indiv$dist), length.out = 100),
+  deg = seq(min(radial_indiv$deg), max(radial_indiv$deg), length.out = 100),
+  # sin_deg = sin_deg,
+  # cos_deg = cos_deg,
   quality_score = 1
 )
+
+#new_data$deg <- angles_deg
+range(new_data$deg)  # Should match radial_indiv$deg range
 new_data$spoke <- factor("s5") # Example, choose appropriately based on your model structure
 new_data$obs <- factor("001") # Assuming a single observation for prediction purposes
 new_data$predicted <- predict(md1, newdata = new_data, type = "response")
 
-# 3. Plotting
-library(ggplot2)
-
-# Assuming new_data is already created and contains the 'predicted' column
 p0 <- ggplot(new_data, aes(x = dist, y = deg)) +
   geom_raster(aes(fill = predicted)) + # Use geom_raster for a continuous field of colors
   scale_fill_gradient(low = "#3B9AB2", high = "#F21A00") + # Color gradient for predictions
   labs(x = "Distance from centre patch (m)", y = "Degrees from downstream", fill = "Fertilisation \n success") +
   geom_contour(aes(z = predicted), breaks = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9), color = "white") + # Draw contour lines based on 'predicted'
   theme_minimal()
-deg_from_s5
 p0 <- p0 + geom_hline(yintercept = c(-78, -52, -26, 0, 26, 52), color = "#E1AF00", lty = 2)
 p0
-#ggsave(p0, filename = '2023palau_fert.tiff',  path = "./plots", device = "tiff",  width = 8, height = 5)  #this often works better than pdf
+
+
+# GAMM --------------------------------------------------------------------
+
+#try gamm
+md1 <- gamm(cbind(suc, tot - suc) ~ s(deg, k = 3) + dist,   random = list(obs = ~1), family = binomial,  method = "REML", verbosePQL = F, 
+            data = radial_indiv)
+AIC(md1)  #k = 3 best
+plot(fitted(md1$gam), residuals(md1$gam)) # fitted vs residuals
+abline(h = 0)
+# Generate new_data for predictions
+new_data <- expand.grid(
+  dist = seq(min(radial_indiv$dist), max(radial_indiv$dist), length.out = 100),
+  deg = seq(min(radial_indiv$deg), max(radial_indiv$deg), length.out = 100),
+  quality_score = 1  # Assuming this is required for consistency, though not used in the model
+)
+
+# Add a dummy observation level for prediction purposes
+new_data$obs <- factor("001")  # Assuming a single observation for prediction
+new_data$predicted <- predict(md1$gam, newdata = new_data, type = "response")
+radial_indiv$residuals <- residuals(md1$gam, type = "deviance")
+
+p0 <- ggplot(new_data, aes(x = dist, y = deg)) +
+  geom_raster(aes(fill = predicted)) +  # 
+  scale_fill_gradient(low = "#3B9AB2", high = "#F21A00") +  # 
+  labs(x = "Distance from centre patch (m)", y = "Degrees from downstream", fill = "Fertilisation \n success") +
+  geom_contour(aes(z = predicted), breaks = seq(0.1, 0.9, by = 0.1), color = "white") +  # 
+  theme_minimal() +
+  geom_hline(yintercept = c(-78, -52, -26, 0, 26, 52), color = "#E1AF00", lty = 2)  #
+# add deviance
+p0 = p0 +  geom_point(data = radial_indiv, aes(x = dist, y = deg, color = residuals), size = 2)+
+  scale_color_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0)
+p0
+
+
+
